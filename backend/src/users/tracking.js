cc3b51b6f92f2a6fc3d3655cb2639e07bb793bcd
Kirill <immotional@aol.com>
2018-12-16 18:44:22 +0000

    refactoring

--- backend/src/users/tracking.js
+++ backend/src/users/tracking.js
@@ -1,15 +1,15 @@
 const uuidv4 = require('uuid/v4');
 
-const USER_ID_COOKIE_NAME = 'user-id';
+const USER_ID_COOKIE = 'user-id';
 
 const addUserIdCookie = (res) => {
   const userId = uuidv4();
-  res.cookie(USER_ID_COOKIE_NAME, userId, {maxAge: 900000, httpOnly: true});
+  res.cookie(USER_ID_COOKIE, userId, {maxAge: 900000, httpOnly: true});
   return userId;
 };
 
 const userIdExtractor = (req, res, next) => {
-  const userIdCookie = req.cookies[USER_ID_COOKIE_NAME];
+  const userIdCookie = req.cookies[USER_ID_COOKIE];
   const userId = userIdCookie || addUserIdCookie(res);
   req.userId = userId;
   next();
